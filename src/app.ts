9adbcfaf0d54a3431120834200de12e81014d6e2
olusola akinsulere <akinsulereolusola@gmail.com>
2024-03-19 08:55:13 +0100

    feat: added comment post user for database model

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import * as bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT);
+
+app.set('env', process.env.NODE_ENV);
+
+app.use(methodOverride());
+
+app.use(routes);
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
+import dataSource from './typeorm-config';
+
+(async () => {
+  dataSource
+    .initialize()
+    .then(() => {
+      // Start express server
+      console.log('Connected to DB successfully.')
+      const port = app.get('port');
+      app.listen(process.env.PORT || 3000, () => {
+
+        console.log(`Service Started at http://localhost:${port}`);
+        console.log('Press CTRL+C to stop\n');
+      });
+
+    }).catch(error => console.error('DB connection failed : ' + error));
+})();
